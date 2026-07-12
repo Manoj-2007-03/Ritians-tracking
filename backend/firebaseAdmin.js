d9101704d031d5e397e9f8d90726263d308ce6bc
manoj.250052@aiml.ritchennai.edu.in <manoj.250052@aiml.ritchennai.edu.in>
2026-07-12 20:39:49 +0530

    Load Firebase credentials from env var for Render deployment

--- backend/firebaseAdmin.js
+++ backend/firebaseAdmin.js
@@ -1,6 +1,9 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getMessaging } = require('firebase-admin/messaging');
-const serviceAccount = require('./firebase-service-account.json');
+
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+  : require('./firebase-service-account.json.json');
 
 const app = initializeApp({ credential: cert(serviceAccount) });
 const messaging = getMessaging(app);
